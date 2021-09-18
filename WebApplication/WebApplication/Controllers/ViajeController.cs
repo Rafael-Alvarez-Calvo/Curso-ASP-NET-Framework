ece09fff93856001abd17afe9faaf79844fe4720
Rafael <rafael.alvarez@hotmail.es>
2021-09-19 01:33:32 +0200

    trips added and some more modifications in html

---
+++ WebApplication/WebApplication/Controllers/ViajeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication.Models;
+
+namespace WebApplication.Controllers
+{
+    public class ViajeController : Controller
+    {
+        // GET: Viaje
+        public ActionResult Index()
+        {
+            List<ViajeCLS> listaViajes = null;
+
+            using (var bd = new BDPasajeEntities())
+            {
+                listaViajes = (from viaje in bd.Viaje
+                                join origen in bd.Lugar
+                                on viaje.IIDLUGARORIGEN equals origen.IIDLUGAR
+                                    join destino in bd.Lugar
+                                    on viaje.IIDLUGARDESTINO equals destino.IIDLUGAR
+                                        join bus in bd.Bus
+                                        on viaje.IIDBUS equals bus.IIDBUS
+                                            select new ViajeCLS
+                                            {
+                                                iidViaje = viaje.IIDVIAJE,
+                                                bus = bus.PLACA,
+                                                origen = origen.NOMBRE,
+                                                destino = destino.NOMBRE,
+
+                                            }).ToList();
+            }
+
+            return View(listaViajes);
+        }
+    }
+}
---
+++ WebApplication/WebApplication/Models/ViajeCLS.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using WebApplication.Models;
+
+namespace WebApplication.Models
+{
+    public class ViajeCLS
+    {
+
+        [Display(Name = "ID Viaje")]
+        public int iidViaje { get; set; }
+
+        [Required]
+        [Display(Name = "Origen")]
+        public int iidLugarOrigen { get; set; }
+
+        [Required]
+        [Display(Name = "Destino")]
+        public int iidLugarDestino { get; set; }
+        
+        [Required]
+        [Display(Name = "Bus")]
+        public int iidBus { get; set; }
+
+        [Required]
+        [Display(Name = "Asientos disponibles")]
+        public int numeroAsientosDisponibles { get; set; }
+        public int bhabilitado { get; set; }
+
+        [Required]
+        [Display(Name = "Precio")]
+        public double precio { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de Viaje")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime fechaViaje { get; set; }
+
+        //////// Operaciones Adicionales ///////
+
+        [Display(Name = "Origen")]
+        public string origen { get; set; }
+        
+        [Display(Name = "Destino")]
+        public string destino { get; set; }
+        
+        [Display(Name = "Bus")]
+        public string bus { get; set; }
+    }
+}
